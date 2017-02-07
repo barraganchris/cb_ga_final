a69931610aea439309d5c4bca8e8817ee71e1900
christopher barragan <barraganchris95@gmail.com>
2017-02-06 18:47:32 -0600

    Made some starter changes to app and index.js

--- cb-final-project/src/App.js
+++ cb-final-project/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// import { Jumbotron } from 'react-bootstrap';
+
 class App extends Component {
   render() {
     return (
