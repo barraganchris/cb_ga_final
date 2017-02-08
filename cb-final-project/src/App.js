e7555fab3dcbf0f033a6fd1345fb09bbeb7598a9
christopher barragan <barraganchris95@gmail.com>
2017-02-08 17:14:25 -0600

    Deleted old CSS and basic React stuff, then nested first components

--- cb-final-project/src/App.js
+++ cb-final-project/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// ABOVE is all imported from React
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer'
+import Landing from './components/Landing';
+// ABOVE are imported Components
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Nav />
+
+          <div className="main-content">
+            <Landing />
+          </div>
+
+        <Footer />
       </div>
     );
   }
